e9f985ec3f051113877ea96a6178cfa6b9de1dd5
Vladimir Boliev <voff.web@gmail.com>
2018-01-11 01:07:42 +0300

    Added new user fields at sonata

--- src/AppBundle/Admin/UserAdmin.php
+++ src/AppBundle/Admin/UserAdmin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
@@ -6,16 +7,27 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\CoreBundle\Form\Type\BooleanType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('email', 'text')
-            ->add('firstName', 'text')
-            ->add('lastName', 'text')
-            ->add('enabled', BooleanType::class)
+            ->add('email', 'text', ['required' => false])
+            ->add('firstName', 'text', ['required' => false])
+            ->add('lastName', 'text', ['required' => false])
+            ->add('enabled', BooleanType::class, ['required' => false])
+            ->add('country', CountryType::class, ['required' => false])
+            ->add('city', TextType::class, ['required' => false])
+            ->add('likeToDo', TextareaType::class, ['label' => 'user.like_to_do', 'required' => false])
+            ->add('expectation', TextareaType::class, ['label' => 'user.expectation', 'required' => false])
+            ->add('experience', TextareaType::class, ['label' => 'user.experience', 'required' => false])
+            ->add('about', TextareaType::class, ['label' => 'user.about', 'required' => false])
+            ->add('aboutFormSkipped', DateTimeType::class, ['label' => 'user.about_form_dkipped_at', 'required' => false])
         ;
     }
 
