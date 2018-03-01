56881e67db23c5a28cc2d29f57e02035c2ad61e9
Vladimir Boliev <voff.web@gmail.com>
2018-03-02 01:45:54 +0300

    About form redesign

--- src/AppBundle/Controller/UserAboutController.php
+++ src/AppBundle/Controller/UserAboutController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -80,5 +81,41 @@ class UserAboutController extends AbstractController
         $em->flush();
 
         return $this->redirectToRoute('homepage');
+    }
+
+    /**
+     * @Route("/specify/about/upload-profile", name="specify_about_upload_avatar")
+     *
+     * @param Request                $request
+     * @param EntityManagerInterface $em
+     * @param UserService            $userService
+     * @param TranslatorInterface    $translator
+     *
+     * @return Response
+     */
+    public function uploadProfileAction(Request $request, EntityManagerInterface $em, UserService $userService, TranslatorInterface $translator)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ($request->request->get('qqfilename')) {
+            try {
+                $uploadedFile = new UploadedFile($_FILES['qqfile']['tmp_name'], $_FILES['qqfile']['name'], mime_content_type($_FILES['qqfile']['tmp_name']));
+                $file = $userService->uploadAvatar($user, $uploadedFile);
+                $user->setProfilePicture($file);
+            } catch (\Exception $e) {
+//                    $form->get('profilePicture')->addError(new FormError($e->getMessage()));
+                return new JsonResponse($e->getMessage(), 400);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            return new JsonResponse(['success' => true, 'picture' => $user->getProfilePicture()]);
+        }
+
+//        return $this->render('user/about/index.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
     }
 }
