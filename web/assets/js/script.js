5004edb049f4715d5c7b04c245b64dbd9d541e74
Vladimir Boliev <voff.web@gmail.com>
2018-04-01 13:52:14 +0300

    lightbox

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -342,9 +342,11 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+// When the user clicks the button, open the modal
+if(btn != undefined) {
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
