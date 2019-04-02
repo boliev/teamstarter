68670ffa1047d7fe3141a838bc8814740cb771c7
Vladimir Boliev <voff.web@gmail.com>
2019-04-02 09:49:44 +0300

    #192: blog more article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,31 +2,35 @@
 
 namespace App\Controller;
 
-use App\Billing\Exception\BillingException;
-use App\Billing\PaymentProcessor;
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Entity\Project;
+use App\Entity\User;
+use App\Form\CommentType;
 use App\Notifications\Notificator;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
 use App\Repository\ProjectRepository;
-use App\Search\ProjectSearcher\ProjectSearcherInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog/{page}", name="blog_list", defaults={"page": 1})
      *
-     * @param ArticleRepository $articleRepository
+     * @param ArticleRepository  $articleRepository
      * @param PaginatorInterface $paginator
-     * @param int $page
-     * @return Response
+     * @param int                $page
      *
+     * @return Response
      */
     public function indexAction(
         ArticleRepository $articleRepository,
@@ -41,7 +45,7 @@ class BlogController extends AbstractController
             $page
         );
 
-        return $this->render('blog/index.html.twig', [
+        return $this->render('blog/list/index.html.twig', [
             'pagination' => $pagination,
         ]);
     }
@@ -49,10 +53,78 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{article}/more", name="blog_more")
      *
-     * @return Response
+     * @param Article           $article
+     * @param CommentRepository $commentRepository
      *
+     * @return Response
      */
-    public function moreAction(Article $article) {
+    public function moreAction(Article $article, CommentRepository $commentRepository)
+    {
+        return $this->render('blog/more/index.html.twig', [
+            'article' => $article,
+            'commentForm' => $this->createForm(CommentType::class)->createView(),
+            'comments' => $commentRepository->getForArticle($article),
+        ]);
+    }
 
+    /**
+     * @Route("/blog/{article}/comment/submit", name="blog_add_comment")
+     *
+     * @param Request                $request
+     * @param EntityManagerInterface $em
+     * @param TranslatorInterface    $translator
+     * @param Article                $article
+     * @param Notificator            $notificator
+     *
+     * @return Response
+     */
+    public function submitComment(
+        Request $request,
+        EntityManagerInterface $em,
+        TranslatorInterface $translator,
+        Article $article,
+        Notificator $notificator
+    ) {
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw new AccessDeniedException();
+        }
+
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $this->createComment($user, $form, $article, $em);
+            $this->addFlash('add-comment-success', $translator->trans('comments.comment_successfully_added'));
+//            $notificator->newProjectComment($project, $comment);
+        }
+
+        return $this->redirectToRoute('blog_more', ['article' => $article->getId()]);
+    }
+
+    /**
+     * @param User                   $user
+     * @param FormInterface          $form
+     * @param Article                $article
+     * @param EntityManagerInterface $em
+     *
+     * @return Comment
+     */
+    private function createComment(User $user, FormInterface $form, Article $article, EntityManagerInterface $em): Comment
+    {
+        $comment = new Comment();
+        $comment->setFrom($user);
+        $comment->setEntity(Comment::ENTITY_ARTICLE);
+        $comment->setToId($article->getId());
+        $comment->setMessage($form->get('comment')->getData());
+        $comment->setRemoved(false);
+        $article->setCommentsCount($article->getCommentsCount() + 1);
+
+        $em->persist($comment);
+        $em->persist($article);
+        $em->flush();
+
+        return $comment;
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\Project;
 use Doctrine\ORM\EntityRepository;
@@ -10,13 +11,23 @@ class CommentRepository extends EntityRepository
 {
     public function getForProject(Project $project)
     {
+        return $this->getForEntity(Comment::ENTITY_PROJECT, $project->getId());
+    }
+
+    public function getForArticle(Article $article)
+    {
+        return $this->getForEntity(Comment::ENTITY_ARTICLE, $article->getId());
+    }
+
+    private function getForEntity(string $name, string $id)
+    {
         return $this->createQueryBuilder('c')
             ->where('c.entity = :entity')
             ->andWhere('c.toId = :id')
             ->andWhere('c.removed = false')
             ->orderBy('c.createdAt')
-            ->setParameter('entity', Comment::ENTITY_PROJECT)
-            ->setParameter('id', $project->getId())
+            ->setParameter('entity', $name)
+            ->setParameter('id', $id)
             ->getQuery()
             ->getResult();
     }
